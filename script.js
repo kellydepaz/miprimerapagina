482a64f8efd17eb52b3739f09a96de207ba3043d
kellydepaz <kellydepaz1494@gmail.com>
2023-11-15 17:08:03 -0500

    mi segunda confirmación de actualización

--- script.js
+++ script.js
@@ -21,18 +21,52 @@ const nombreCompleto = document.getElementById("nombre");
 const email = document.getElementById("email");
 const mensaje = document.getElementById("mensaje");
 const boton = document.getElementById("boton");
-const alertaNombreIncompleto = document.getElementById("alertas");
+const alertaNombreIncompleto = document.getElementById("alerta-nombre");
+const alertaCorreoDiligenciado = document.getElementById("alerta-email");
+const alertaMensajeDiligenciado = document.getElementById("alerta-mensaje");
+const alertaFormularioEnviado = document.getElementById("alerta-enviado");
+
+
 
 //Hacer funcion 
 function verificarInformacion () {
-    
+
     if (nombreCompleto.value.length < 4) {
-        alertaNombreIncompleto.innerText = "Nombre inválido, debes ser mayor a 4 caractéres";
+        alertaNombreIncompleto.innerText = "Nombre inválido, debe ser mayor a 4 caractéres";
+       
+    }  else {
+        alertaNombreIncompleto.innerText = "";    
+        
+    }
+
+    if (email.value == "" ) {
+        alertaCorreoDiligenciado.innerText = "Debes diligenciar un correo válido";
+        
+    } else {
+        alertaCorreoDiligenciado.innerText = "";
+       
+    }
+
+    if (mensaje.value == "" ) {
+        alertaMensajeDiligenciado.innerText = "Debes colocar un mensaje";
+        
+    } else {
+        alertaMensajeDiligenciado.innerText =  "";
+       
+    }
+    
+    if (nombreCompleto.value.length > 4 && email.value !== "" && mensaje.value !== "" ) {
+        alertaFormularioEnviado.innerText = "Su información ha sido enviada";
         nombreCompleto.value = "";
-    }
+        email.value = "";
+        mensaje.value = "";
+    } 
+
     else {
-        alertaNombreIncompleto.innerText = "";
+        alertaFormularioEnviado.innerText = "";
+        
     }
+
 }
 
 //Escuchar al evento 
